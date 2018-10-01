61be001f66612fd3c1eff898d3a000e4888c1f2f
olivergate <oliver.kg2@gmail.com>
2018-10-01 10:31:07 +0100

    initial commit, front end added and roughly styled, hero_statement automatically input

--- cms.php
+++ cms.php
@@ -5,4 +5,27 @@
  * Date: 01/10/2018
  * Time: 09:02
  */
+$hero_statement_new = $_POST
 
+?>
+
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="cms.css" />
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Stalemate" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Italiana" rel="stylesheet">
+
+    <title>Content Management system</title>
+</head>
+<body>
+    <main>
+        <div class="form_container">
+            <form action="index.php" method="post">
+                <input type="text" name="hero_statement" value="<?php echo $hero_statement; ?>" />
+                <input type="submit"/>
+            </form>
+        </div>
+    </main>
+</body>
