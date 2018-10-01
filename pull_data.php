5e1e043d4bbd44eaaba80da48090b6f737002059
olivergate <oliver.kg2@gmail.com>
2018-10-01 09:42:27 +0100

    functions docbloc created and changed output to return statement

--- pull_data.php
+++ pull_data.php
@@ -2,10 +2,16 @@
 $db = new PDO ($hostname, $dbusername);
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-function text_input(string $location_description, $db) {
+/** function that pulls data from the database and returns it as a string for use on the front end
+ * @param string $location_description - this correlate to the database entry under "location_description" in the text_input section.
+ * @param $db the linked object that holds the database
+ *
+ * @return string The content fromt he database as a string to be echoes into the html.
+ */
+function text_input(string $location_description, $db) : string {
     $query = $db->prepare("SELECT `content` FROM `text_input` WHERE `location_description`='$location_description';");
     $query -> execute();
     $result = $query->fetch();
-    echo $result['content'];
+    return $result['content'];
 }
 
