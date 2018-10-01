01dfcb41c86904da033d03e8dbaf18fbb8210eae
olivergate <oliver.kg2@gmail.com>
2018-10-01 15:23:45 +0100

    all things resolved

--- pull_data.php
+++ pull_data.php
@@ -1,16 +1,18 @@
 <?php
 require 'password.php';
 
-/** function that pulls data from the database and returns it as a string for use on the front end
+/** function that pulls data from the database and returns it as an array to mine data from
  *
- * @param string $key_field_name. Selects the fieldname that is being slected from whch will represent the key.
- * @param string $fieldName. Selects the fieldname that is being slected from
+ * @param string $key_field_name. Selects the field name that is being selected from which will represent the key.
+ * @param string $fieldName. Selects the field name that is being selected from
  * @param PDO $db linked object that holds the database
  *
  * @return array The content as an array
  */
 function content_collect(string $key_field_name, string $fieldName, PDO $db) : array {
     $query = $db->prepare("SELECT `$key_field_name`, `$fieldName` FROM `text_input`;");
+    $query->bindParam('key_name', $key_field_name);
+    $query->bindParam('field_name', $fieldName);
     $query -> execute();
     $result = $query->fetchAll();
     return $result;
@@ -18,7 +20,7 @@ function content_collect(string $key_field_name, string $fieldName, PDO $db) : a
 
 /**This function sifts through an array of database results and picks out a value according to that location
  *
- * @param string $location_id This is the description of where the content will go and correlates tot he database entry location_description
+ * @param string $location_id This is the description of where the content will go and correlates to the database entry location_description
  * @param array $database_pull this is the array which holds the pull from the database 'portfolio'
  *
  * @return string pull out the text which is in the database
