05bb1118af00ba871068c84fa131fb936b2c7d2d
olivergate <oliver.kg2@gmail.com>
2018-10-02 10:24:12 +0100

    initial commit of pre-completed functions

---
+++ sanitize_functions.php
@@ -0,0 +1,32 @@
+<?php
+
+/** Function that really makes sure that the string that is being input into the function is an actual string and gets
+ * rid of any special characters that could effect php or html or sql.
+ *
+ * @param string $input_string the string which requires sensitization
+ * @return bool false if the sensitization fails
+ */
+function clean_string(string $input_string) : bool {
+    if (is_string($input_string)===true && strlen($input_string)<255) {
+        $sanitised_string = filter_var($input_string, FILTER_SANITIZE_STRING);
+        trim($sanitised_string);
+        return $sanitised_string;
+    } else return false;
+}
+
+
+/**Function that cleans a string that is meant to contain a date. Method is that by exploding the array, it measure the
+ * length of each part of the array and returns either a 4-2-2 result as a string or a false statement. Extra functionality required
+ * to clean the char set of anything but numeric values and '-' marks.
+ *
+ * @param string $input_date input comes from a form which is set to date input.
+ * @return array|bool the return is either a string of format 4-2-2 or a false statement
+ */
+function clean_date(string $input_date) {
+    $exploded_array = explode('-', $input_date);
+    if (strlen($exploded_array[0])==4 && strlen($exploded_array[1])==2 &&strlen($exploded_array[2])==2) {
+        $clean_date = [$exploded_array[0] . '-' . $exploded_array[1] . '-' . $exploded_array[2], true];
+        return $clean_date;
+    } else
+        return false;
+}
