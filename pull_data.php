069fac6062e4ebf482fe5d0e5332dbd5cf0ae06b
olivergate <oliver.kg2@gmail.com>
2018-10-04 11:52:09 +0100

    issues resolved with extra content

--- pull_data.php
+++ pull_data.php
@@ -10,21 +10,3 @@ $about_me1 = content_picker('about_me1', $content_array);
 
 $portfolio_db = portfolio_collect($db);
 
-foreach ($portfolio_db as $row) {
-    echo '<br>delete =>' . $row['delete'];
-    echo '<br>image_file_name =>' . $row['image_file_name'];
-    echo '<br>project_name =>' . $row['project_name'];
-    echo '<br>project_url =>' . $row['project_url'];
-    echo '<br>hover_text =>' . $row['hover_text'];
-}
-
-$example_array = [
-    [
-        'delete' => 1,
-        'image_file_name' => '..img/mayden.jpg',
-        'project_name' => 'CSS Logo',
-        'project_url' => 'https://dev.maydenacademy.co.uk/students/2018/oliverg/Logo/',
-        'hover_text' => 'Have a look'
-    ]
-];
-display_portfolio($example_array);
