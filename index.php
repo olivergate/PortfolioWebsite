95b78b36faa038eebfb373e22dbc2bcb4e351a34
olivergate <oliver.kg2@gmail.com>
2018-11-21 16:46:54 +0000

    changed the layout and the background images for the whol site

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 require 'pull_data.php';
-session_start();
 ?>
 
 
@@ -12,64 +11,65 @@ session_start();
     <link href="https://fonts.googleapis.com/css?family=Stalemate" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Italiana" rel="stylesheet">
 
-    <title>OLIVER GATE CODING</title>
+    <title>OLIVER GATE Web-Dev</title>
 </head>
 <body>
+<nav>
+    <img src="img/logo.png" class="logo"/>
+    <div id="burger_container" class="burger_container">
+        <img  id="burger"  class="burger_selector" src="img/burger2.png">
+        <div id="drop_down" class="drop_down">
+            <div class="nav_links">
+                <div class="buttons">
+                    <a href="#home">Home</a>
+                </div>
+                <div class="buttons">
+                    <a href="#portfolioId">Work</a>
+                </div>
+                <div class="buttons">
+                    <a href="#about">About</a>
+                </div>
+            </div>
+        </div>
+    </div>
 
+    <div class="nav_container">
+        <div class="logo_name">
+            OKG Web Development
+        </div>
+        <div class="nav_links">
+            <div class="buttons">
+                <a href="#home">Home</a>
+            </div>
+            <div class="buttons">
+                <a href="#portfolioId">Work</a>
+            </div>
+            <div class="buttons">
+                <a href="#about">About</a>
+            </div>
+        </div>
+    </div>
+
+</nav>
      <div id="home" class="home_page">
          <div class="home_container">
-            <div class="hero_statement">
-
-                <h2>Oliver Kaikane Gate</h2><p>Full-stack Web development</p>
-                        <p><?php echo $hero_statement; ?></p>
-
-            </div>
-             <div class="buttons"> <a href="#about"> How can I help </a> </div>
+<!--            <div class="hero_statement">-->
+<!--                <h2>Oliver Kaikane Gate</h2><p>Full-stack Web development</p>-->
+<!--                        <p>--><?php //echo $hero_statement; ?><!--</p>-->
+<!--            </div>-->
+<!--             <div class="buttons"><a href="#about"> How can I help </a></div>-->
         </div>
      </div>
-     <nav>
-         <img src="img/logo.png" class="logo"/>
-         <div id="burger_container" class="burger_container">
-             <img  id="burger"  class="burger_selector" src="img/burger2.png">
-             <div id="drop_down" class="drop_down">
-                 <div class="nav_links">
-                     <div class="buttons">
-                         <a href="#home">Home</a>
-                     </div>
-                     <div class="buttons">
-                         <a href="#portfolioId">Work</a>
-                     </div>
-                     <div class="buttons">
-                         <a href="#about">About</a>
-                     </div>
-                 </div>
-             </div>
-         </div>
 
-         <div class="nav_container">
-             <div class="logo_name">
-                 OKG Web Development
-             </div>
-             <div class="nav_links">
-                 <div class="buttons">
-                     <a href="#home">Home</a>
-                 </div>
-                 <div class="buttons">
-                     <a href="#portfolioId">Work</a>
-                 </div>
-                 <div class="buttons">
-                     <a href="#about">About</a>
-                 </div>
-             </div>
-         </div>
-
-     </nav>
      <section id="about" >
          <div class="body_container">
              <div class="col70_col30">
                  <h2>About</h2>
                  <p><?php echo $about_me1; ?><p>
-                     Oliver is a junior developer training at Mayden Academy, Bath. He enjoys long walks in the woods and occasionally a calvados by the fireside. One day he hopes to develop for a friendly local independent coding company to hone his skills and develop professionaly within. </p>
+                     Oliver is a junior developer training at Mayden Academy, Bath. He enjoys long
+                     walks in the woods and occasionally a calvados by the fireside.
+                     One day he hopes to develop for a friendly local tech
+                     company where he can develop his abilities. </p>
                  </p>
                  <h3>Skill set</h3>
                  <p>
